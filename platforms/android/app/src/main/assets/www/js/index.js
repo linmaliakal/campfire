93eb1915bb4fefa5ab01d29faf76771c26111223
Jrara <jaranguren@luc.edu>
2018-10-31 18:03:09 -0500

    Moved the functions into the JS file

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,46 +1,63 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function switchTest(){
+  var testBox = document.getElementById("GeoLocation");
+  var text = document.getElementById("test").innerHTML;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+if (testBox.checked == true){
+  document.getElementById("test").innerHTML = "Checked";
+}
+else{
+  document.getElementById("test").innerHTML = "UnChecked";
 
-        console.log('Received Event: ' + id);
-    }
-};
+}
+}
 
-app.initialize();
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(onSuccess,onFail, {
+    timeout: 15000,
+    enableHighAccuracy: true
+    });
+}
+
+function onSuccess(location) {
+    var myLatitude = location.coords.latitude;
+    var myLongitude = location.coords.longitude;
+    var mySpeed = location.coords.speed;
+    var myTimeStamp = location.timestamp;
+
+    document.getElementById("latitude").innerHTML = "My Latitude is " + myLatitude;
+    document.getElementById("longitude").innerHTML = "My Longitude is " + myLongitude;
+    document.getElementById("speed").innerHTML = "My Speed is " + mySpeed;
+    document.getElementById("timestamp").innerHTML = "My TimeStamp is " + myTimeStamp;
+}
+
+function onFail(error) {
+  document.getElementById("speed").innerHTML = "Please Enable GeoLocation";
+
+}
+
+function getWatchID() {
+  watchID = navigator.geolocation.watchPosition(onSuccess, onFail,
+  {
+        enableHighAccuracy: true
+      });
+
+      return watchID
+}
+
+function toggleWatchID(){
+   var testBox = document.getElementById("GeoLocation");
+   if (testBox.checked == true) {
+     watchID = getWatchID();
+     console.log("watchID = "+watchID);
+     document.getElementById("test").innerHTML ="Works";
+   }
+
+   else {
+      navigator.geolocation.clearWatch(watchID);
+      console.log("clear watch..."+watchID);
+      document.getElementById("test").innerHTML = "else"
+   }
+
+}
