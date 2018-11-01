a514d0400c3cf63759ab8b0bd94e010bc6963a3d
Linette V Maliakal <lmaliakal21@gmail.com>
2018-10-31 22:34:46 -0500

    initializing Firebase & testing with it
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -44,3 +44,19 @@ var app = {
 };
 
 app.initialize();
+
+//for Drop Down Menu of Interests Page
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
