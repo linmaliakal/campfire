f308ae22a3309b3316b9b05fdc2cec7cd4573a4b
Linette V Maliakal <lmaliakal21@gmail.com>
2018-12-12 22:30:28 -0600

    styled addEvents page, testing with adding event info to Firebase
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

---
+++ platforms/android/app/build/intermediates/assets/debug/www/js/addevent.js
@@ -0,0 +1,37 @@
+function GatherEventInfo() {
+  var event_name;
+  var date;
+  var address;
+  var interests;
+
+  function GatherEventInfo(){}
+
+  function GatherEventInfo(var event_name, var date, var address, var interests) {
+    this.event_name = event_name;
+    this.date = date;
+    this.address = address;
+    this.interests = interests;
+  }
+
+  function Dictionary<string, Object> ToDictonary() {
+    Dictionary<string, Object> result = new Dictionary<string, Object>();
+    result["event_name"] = event_name;
+    result["date"] = date;
+    result["address"] = address;
+    result["interests"] = interests;
+
+    return result;
+  }
+}
+
+function addEvent(var event_name, var date, var address, var interests) {
+  var key = mDatabase.Child("event_name").Push().Key;
+  GatherEventInfo entry = new GatherEventInfo(event_name, date, address, interests);
+  Dictionary<string, Object> entryValues = entry.ToDictionary();
+
+  Dictionary<string, Object> childUpdates = new Dictionary<string, Object>();
+  childUpdates["/scores/" + key] = entryValues;
+  childUpdates["/user-scores/" + userId + "/" + key] = entryValues;
+
+  mDatabase.UpdateChildrenAsync(childUpdates);
+}
